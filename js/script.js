b574844a36c57fce435de2fb2916dfbeaad93f46
click-SK <105132436+click-SK@users.noreply.github.com>
2022-05-15 02:06:37 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-
+// ====================== typing animation===================
+var typed = new Typed(".typing", {
+  strings: [" ", "Web Desinger", "Web Developer", "Graphic Designer"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
