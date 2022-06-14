4f84279759bfb1c2d58b5df486a56ebbfeba91c9
click-SK <105132436+click-SK@users.noreply.github.com>
2022-06-14 12:06:01 +0300

    Add files via upload

---
+++ projects/Magic Navigation Menu.js
@@ -0,0 +1,12 @@
+// ====================== typing animation===================
+var typed = new Typed(".typing-2", {
+  strings: [
+    " ",
+    "This is Magic Navigation Menu project",
+    "Indicator using Html CSS and Javascript",
+    "You can rate!",
+  ],
+  typeSpeed: 70,
+  BackSpeed: 60,
+  loop: true,
+});
