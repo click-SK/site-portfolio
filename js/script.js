7d3841275d5da0ae7e279a355be4d8a6f346f921
click-SK <105132436+click-SK@users.noreply.github.com>
2022-06-14 15:40:45 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -86,49 +86,49 @@ function asideSectionTogglerBtn() {
   }
 }
 
-// send email//
-("use strict");
+// // send email//
+// ("use strict");
 
-document.addEventListener("DOMContentLoader", function () {
-  const form = document.getElementById("form");
-  form.addEventListener("submit", formSend);
+// document.addEventListener("DOMContentLoader", function () {
+//   const form = document.getElementById("form");
+//   form.addEventListener("submit", formSend);
 
-  async function formSend(e) {
-    e.preventDefault();
+//   async function formSend(e) {
+//     e.preventDefault();
 
-    let error = formValidate(form);
-  }
+//     let error = formValidate(form);
+//   }
 
-  function formValidate(form) {
-    let error = 0;
-    let formReq = document.querySelectorAll("._req");
+//   function formValidate(form) {
+//     let error = 0;
+//     let formReq = document.querySelectorAll("._req");
 
-    for (let index = 0; index < formReq.length; index++) {
-      const input = formReq[index];
-      formRemoveError(input);
-      if (input.classList.contains("_email")) {
-        if (emailTest(input)) {
-          formAddError(input);
-          error++;
-        }
-      } else {
-        if (input.value === "") {
-          formAddError(input);
-          error++;
-        }
-      }
-    }
-  }
+//     for (let index = 0; index < formReq.length; index++) {
+//       const input = formReq[index];
+//       formRemoveError(input);
+//       if (input.classList.contains("_email")) {
+//         if (emailTest(input)) {
+//           formAddError(input);
+//           error++;
+//         }
+//       } else {
+//         if (input.value === "") {
+//           formAddError(input);
+//           error++;
+//         }
+//       }
+//     }
+//   }
 
-  function formAddError(input) {
-    input.parentElement.classList.add("_error");
-    input.classList.add("_error");
-  }
-  function formRemoveError(input) {
-    input.parentElement.classList.remove("_error");
-    input.classList.remove("_error");
-  }
-  function emailTest(input) {
-    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\..w{2,8})+$/.test(input.value);
-  }
-});
+//   function formAddError(input) {
+//     input.parentElement.classList.add("_error");
+//     input.classList.add("_error");
+//   }
+//   function formRemoveError(input) {
+//     input.parentElement.classList.remove("_error");
+//     input.classList.remove("_error");
+//   }
+//   function emailTest(input) {
+//     return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\..w{2,8})+$/.test(input.value);
+//   }
+// });
