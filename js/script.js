75315486f0dbd1417616b8b29f1c1628f78aaf47
click-SK <105132436+click-SK@users.noreply.github.com>
2022-06-14 12:05:38 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -14,80 +14,121 @@ const nav = document.querySelector(".nav"),
   totalSection = allSection.length;
 for (let i = 0; i < totalNavList; i++) {
   const a = navList[i].querySelector("a");
-  a.addEventListener("click", function(){
-  // for(let i=0; i<totalSection; i++)
-  // {
-  //   allSection[i].classList.remove("back-section");
-  // }
-  removeBackSection();
-    for(let j=0; j<totalNavList; j++)
-    {
-      if(navList[j].querySelector("a").classList.contains("active"))
-      {
+  a.addEventListener("click", function () {
+    // for(let i=0; i<totalSection; i++)
+    // {
+    //   allSection[i].classList.remove("back-section");
+    // }
+    removeBackSection();
+    for (let j = 0; j < totalNavList; j++) {
+      if (navList[j].querySelector("a").classList.contains("active")) {
         addBackSection(j);
         // allSection[j].classList.add("back-section");
       }
       navList[j].querySelector("a").classList.remove("active");
     }
-    this.classList.add("active")
+    this.classList.add("active");
     showSection(this);
-    if(window.innerWidth < 1200)
-    {
+    if (window.innerWidth < 1200) {
       asideSectionTogglerBtn();
     }
-  })
+  });
 }
-function removeBackSection()
-{
-  for(let i=0; i<totalSection; i++)
-  {
+function removeBackSection() {
+  for (let i = 0; i < totalSection; i++) {
     allSection[i].classList.remove("back-section");
   }
 }
-function addBackSection(num){
+function addBackSection(num) {
   allSection[num].classList.add("back-section");
 }
 
-function showSection(element){
-  for(let i=0; i<totalSection; i++)
-  {
+function showSection(element) {
+  for (let i = 0; i < totalSection; i++) {
     allSection[i].classList.remove("active");
   }
   const target = element.getAttribute("href").split("#")[1];
-  document.querySelector("#" + target).classList.add("active")
+  document.querySelector("#" + target).classList.add("active");
 }
 
-function updateNav(element){
-  for(let i=0; i<totalNavList; i++)
-  {
+function updateNav(element) {
+  for (let i = 0; i < totalNavList; i++) {
     navList[i].querySelector("a").classList.remove("active");
     const target = element.getAttribute("href").split("#")[1];
-    if(target === navList[i].querySelector("a").getAttribute("href").split("#")[1])
-    {
+    if (
+      target ===
+      navList[i].querySelector("a").getAttribute("href").split("#")[1]
+    ) {
       navList[i].querySelector("a").classList.add("active");
     }
   }
 }
 
-document.querySelector(".hire-me").addEventListener("click", function(){
+document.querySelector(".hire-me").addEventListener("click", function () {
   const sectionIndex = this.getAttribute("data-section-index");
   console.log(sectionIndex);
   showSection(this);
   updateNav(this);
   removeBackSection();
   addBackSection(sectionIndex);
-})
+});
 
 const navTogglerBtn = document.querySelector(".nav-toggler"),
   aside = document.querySelector(".aside");
-  navTogglerBtn.addEventListener("click", () =>{
-    asideSectionTogglerBtn();
-  })
-  function asideSectionTogglerBtn(){
-    aside.classList.toggle("open");
-    navTogglerBtn.classList.toggle("open");
-    for(let i=0; i<totalSection; i++)
-    {
-      allSection[i].classList.toggle("open");
+navTogglerBtn.addEventListener("click", () => {
+  asideSectionTogglerBtn();
+});
+function asideSectionTogglerBtn() {
+  aside.classList.toggle("open");
+  navTogglerBtn.classList.toggle("open");
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.toggle("open");
+  }
+}
+
+// send email//
+("use strict");
+
+document.addEventListener("DOMContentLoader", function () {
+  const form = document.getElementById("form");
+  form.addEventListener("submit", formSend);
+
+  async function formSend(e) {
+    e.preventDefault();
+
+    let error = formValidate(form);
+  }
+
+  function formValidate(form) {
+    let error = 0;
+    let formReq = document.querySelectorAll("._req");
+
+    for (let index = 0; index < formReq.length; index++) {
+      const input = formReq[index];
+      formRemoveError(input);
+      if (input.classList.contains("_email")) {
+        if (emailTest(input)) {
+          formAddError(input);
+          error++;
+        }
+      } else {
+        if (input.value === "") {
+          formAddError(input);
+          error++;
+        }
+      }
     }
   }
+
+  function formAddError(input) {
+    input.parentElement.classList.add("_error");
+    input.classList.add("_error");
+  }
+  function formRemoveError(input) {
+    input.parentElement.classList.remove("_error");
+    input.classList.remove("_error");
+  }
+  function emailTest(input) {
+    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\..w{2,8})+$/.test(input.value);
+  }
+});
